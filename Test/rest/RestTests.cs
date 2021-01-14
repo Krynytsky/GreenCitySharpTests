2fc6c19a9428907a0aada2ed8c8595e778e7371e
Krynytsky <yuri.krynytsky@gmail.com>
2021-01-14 22:52:52 +0200

    #deleted whitespaces

--- Test/rest/RestTests.cs
+++ Test/rest/RestTests.cs
@@ -11,12 +11,12 @@ namespace Test.rest
 
         [TestCase(HttpStatusCode.OK, TestName = "Check status code for NL zip code 7411")]
         [TestCase(HttpStatusCode.NotFound, TestName = "Check status code for LV zip code 1050")]
+
         [Test]
         public void MethodGetStatusCodeTest(HttpStatusCode expectedHttpStatusCode)
         {
             IRestClient restClient = new RestClient(baseUrl);
             IRestRequest restRequest = new RestRequest("nl/3ff5", Method.GET);
-
             IRestResponse restResponse = restClient.Execute(restRequest);
 
             Assert.That(restResponse.StatusCode, Is.EqualTo(expectedHttpStatusCode));
