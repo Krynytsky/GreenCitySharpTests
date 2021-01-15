dd12ee56b1f7be2252649c10e0080880512c804d
Krynytsky <yuri.krynytsky@gmail.com>
2021-01-16 01:47:52 +0200

    Added test for verify that user can click the sign in button; Added headless mode as a ChromeOption

---
+++ Test/GreenCityTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace GreenCityTests
+{
+    public class GrenCityTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("--headless");
+            driver = new ChromeDriver(option);
+            driver.Navigate().GoToUrl("https://ita-social-projects.github.io/GreenCityClient/#/");
+        }
+
+        [Test]
+        public void sighInBtnTest()
+        {
+            IWebElement signInBtn = driver.FindElement(By.XPath("//a[@role='sign in']"));
+            signInBtn.Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement welcomeTitle = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[contains(text(),' Welcome back!')]")));
+            string expected = "Welcome back!";
+            Assert.AreEqual(expected, welcomeTitle.Text);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
