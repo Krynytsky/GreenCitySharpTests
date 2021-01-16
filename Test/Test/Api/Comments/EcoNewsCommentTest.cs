9a7240aff38456eb9bf52f99bf876327daeaf371
Krynytsky <yuri.krynytsky@gmail.com>
2021-01-16 23:44:03 +0200

    Changed  structure

--- Test/Test/Api/Comments/EcoNewsCommentTest.cs
+++ Test/Test/Api/Comments/EcoNewsCommentTest.cs
@@ -1,18 +1,17 @@
 ﻿using NUnit.Framework;
 using RestSharp;
 using System;
+using System.IO;
 using System.Net;
 
-namespace Test.Test.Api.OwnSequrity.SignIn
+namespace GreenCity.Test.Api.Comments.SignIn
 {
     class EcoNewsCommentTest
     {
-
        [Test]
         public void CreationEcoNewsCommentTest()
         {
             var client = new RestClient("https://greencity.azurewebsites.net/econews/comments/8790");
-            client.Timeout = -1;
             var request = new RestRequest(Method.POST);
             request.AddHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcnV2YXRAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYxMDgxODc4OCwiZXhwIjoxNjEwODI1OTg4fQ.yk9K84KHuBlr0xnHTvMh7skimf2ZlHA_jORi5tHGm_Q");
             request.AddHeader("Content-Type", "application/json");
---
+++ Test/Test/Api/OwnSequrity/LoginTests/SignInTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+
+
+namespace Test.Test.Api.OwnSequrity.LoginTests
+{
+    class SignInTests
+    {
+        [Test]
+        public void SignInApiTest()
+        {
+            var client = new RestClient("https://greencity.azurewebsites.net/ownSecurity/signIn");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", "{\n    \"email\": \"pruvat@gmail.com\",\n    \"password\": \"1Test@test\"\n}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
