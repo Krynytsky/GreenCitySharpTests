ff1db168d8e3cd6a630b48927e81fa8d0e985f56
Krynytsky <yuri.krynytsky@gmail.com>
2021-01-17 08:46:07 +0200

    changed namespace

--- Test/Test/Api/Comments/EcoNewsCommentTest.cs
+++ Test/Test/Api/Comments/EcoNewsCommentTest.cs
@@ -4,7 +4,7 @@ using System;
 using System.IO;
 using System.Net;
 
-namespace GreenCity.Test.Api.Comments.SignIn
+namespace GreenCity.Test.Api.Comments
 {
     class EcoNewsCommentTest
     {
@@ -15,7 +15,7 @@ namespace GreenCity.Test.Api.Comments.SignIn
             IRestRequest request = new RestRequest(Method.POST);
             request.AddHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcnV2YXRAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTYxMDgxODc4OCwiZXhwIjoxNjEwODI1OTg4fQ.yk9K84KHuBlr0xnHTvMh7skimf2ZlHA_jORi5tHGm_Q");
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", "{\n  \"parentCommentId\": 0,\n  \"text\": \"comment from postman\"\n}", ParameterType.RequestBody);
+            request.AddParameter("application/json", "{\n\"parentCommentId\": 0,\n\"text\": \"comment from postman\"\n}", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
         }
